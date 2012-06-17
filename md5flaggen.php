10b4ea56b06e78513f4cf107d1a0986c43019288
Ben Nuttall <ben@bennuttall.com>
2012-06-17 21:25:43 +0100

    Added line breaks

--- md5flaggen.php
+++ md5flaggen.php
@@ -8,6 +8,6 @@ $col1 = substr($md5,0,6);
 $col2 = substr($md5,6,6);
 $col3 = substr($md5,12,6);
 
-echo 	"<div style='float:left;width:100px;height:200px;background:#{$col1}'>&nbsp;</div>" .
-	"<div style='float:left;width:100px;height:200px;background:#{$col2}'>&nbsp;</div>" .
+echo 	"<div style='float:left;width:100px;height:200px;background:#{$col1}'>&nbsp;</div>\n" .
+	"<div style='float:left;width:100px;height:200px;background:#{$col2}'>&nbsp;</div>\n" .
 	"<div style='float:left;width:100px;height:200px;background:#{$col3}'>&nbsp;</div>";
